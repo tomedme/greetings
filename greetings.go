c04bb5ce5a382956631a66dfa21f495d85a26d70
tom <tom@binlid.net>
2021-01-06 09:45:57 +0900

    add error checking

--- greetings.go
+++ greetings.go
@@ -1,10 +1,18 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // Hello returns a greeting for the named person
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	// If no name given, return error with a message
+	if name == "" {
+		return "", errors.New("error: empty name")
+	}
+
 	// Return a greeting that embeds the name in a message
 	message := fmt.Sprintf("Hi %v!", name)
-	return message
+	return message, nil
 }
