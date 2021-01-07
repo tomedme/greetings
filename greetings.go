e66744ea3b67deae366a44084d1ca9f8c20ddfc2
tom <tom@binlid.net>
2021-01-07 18:33:18 +0900

    add multiple hellos

--- greetings.go
+++ greetings.go
@@ -20,6 +20,26 @@ func Hello(name string) (string, error) {
 
 }
 
+// Hellos returns a map
+func Hellos(names []string) (map[string]string, error) {
+	// A map to associate names with messages
+	messages := make(map[string]string)
+
+	// Loop through the received slice of names, calling
+	// the Hello function to get a message for each name
+	for _, name := range names {
+		message, err := Hello(name)
+
+		if err != nil {
+			return nil, err
+		}
+
+		messages[name] = message
+	}
+
+	return messages, nil
+}
+
 func init() {
 	rand.Seed(time.Now().UnixNano())
 }
