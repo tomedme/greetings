b2d3d88663e53098a66ba54893ca4b03b3289e79
tom <tom@binlid.net>
2021-01-08 10:36:09 +0900

    add tests

--- greetings.go
+++ greetings.go
@@ -16,6 +16,7 @@ func Hello(name string) (string, error) {
 
 	// Return a greeting that embeds the name in a message
 	message := fmt.Sprintf(randomFormat(), name)
+	// message := fmt.Sprintf(randomFormat())
 	return message, nil
 
 }
